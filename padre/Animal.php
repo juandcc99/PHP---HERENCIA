c76e519b9af517e1c4492f4cc41aa06ea0efb428
juandcc99 <93380707+juandcc99@users.noreply.github.com>
2022-02-03 10:55:50 +0100

    Create Animal.php

---
+++ padre/Animal.php
@@ -0,0 +1,12 @@
+<?php
+
+abstract class Animal{
+    
+    public function comer(){
+        echo "<p>Animal Comiendo</p>";
+    }
+    protected function dormir(){
+        echo "<p>Animal Durmiendo</p>";
+    }
+
+}
